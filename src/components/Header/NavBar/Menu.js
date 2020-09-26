8f3e9ecbf73e3b374e0cf5ab1e55892e19c7d76c
angeleraser <angeleraser@gmail.com>
2020-09-26 17:39:02 -0400

    Fix PCButton action while isFetching on Home page

--- src/components/Header/NavBar/Menu.js
+++ src/components/Header/NavBar/Menu.js
@@ -1,29 +1,36 @@
 import React from "react";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { PokemonContext } from "../../../PokemonContext/PokemonContext";
 import { pcIcon } from "../../../svg-icons";
 import { types } from "../../../types/types";
 import "./css/Menu.css";
 import { SearchPokemon } from "./SearchPokemon";
 export const Menu = ({ isHidden, hiddenMenu }) => {
-  const { arena, arenaDispatch } = useContext(PokemonContext);
+  const {
+    arena,
+    arenaDispatch,
+    state: { homePage },
+  } = useContext(PokemonContext);
+  const history = useHistory();
   return (
     <div
       className={`menu w-full flex flex-col items-end absolute ${
         !isHidden ? "active" : ""
       }`}>
       <SearchPokemon hiddenMenu={hiddenMenu} />
-      <Link
+      <button
         onClick={() => {
           arena.enabled && arenaDispatch({ type: types.disableArena });
+          !homePage.status.current &&
+            !homePage.status.isFetching &&
+            history.replace("/pc");
           hiddenMenu(true);
         }}
-        to="/pc"
         className="my-team">
         pc
         {pcIcon}
-      </Link>
+      </button>
     </div>
   );
 };
